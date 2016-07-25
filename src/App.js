7d4d292b769b09f277460bb057ee9cf52d864ef8
rafaelkyrdan <rafaelkyrdan@gmail.com>
2016-07-25 17:41:29 +0300

    added index route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
 
-const Home = (props) => <div><h1>Home</h1><Links/>{props.children}</div>
-const About = (props) => <div><h1>About</h1>{props.children}</div>
+const Outer = (props) => <div><h1>Our site</h1><Links/>{props.children}</div>
+const About = () => <div><h1>About</h1></div>
 const Contact = () => <div><h1>Contact</h1></div>
 
 const Links = () =>
   <ul class="nav nav-tabs">
     <li role="presentation"><Link to="/">Home</Link></li>
     <li role="presentation"><Link to="/about">About</Link></li>
-    <li role="presentation"><Link to="/about/contact">Contact</Link></li>
+    <li role="presentation"><Link to="/contact">Contact</Link></li>
   </ul>
 
 class App extends React.Component {
@@ -17,10 +17,9 @@ class App extends React.Component {
   render(){
     return(
       <Router history={ hashHistory }>
-        <Route path="/" component={Home}>
-          <Route path="about" component={About}>
-            <Route path="contact" component={Contact}></Route>
-          </Route>
+        <Route path="/" component={Outer}>
+          <IndexRoute component={About}></IndexRoute>
+          <Route path="contact" component={Contact}></Route>
         </Route>
       </Router>
     )
