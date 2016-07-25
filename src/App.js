f9a0f21c88fe42d54f33f06a18ed166e0365dd9b
rafaelkyrdan <rafaelkyrdan@gmail.com>
2016-07-25 16:49:37 +0300

    added first: router, route, link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { Router, Route, Link, hashHistory } from 'react-router';
 
-const App = () => <h1>Hello world!</h1>
+const Home = () => <div><h1>Home</h1><Links/></div>
+const About = () => <div><h1>About</h1><Links/></div>
+const Contact = () => <div><h1>Contact</h1><Links/></div>
+
+const Links = () =>
+  <ul class="nav nav-tabs">
+    <li role="presentation" class="active"><Link to="/">Home</Link></li>
+    <li role="presentation"><Link to="/about">About</Link></li>
+    <li role="presentation"><Link to="/contact">Contact</Link></li>
+  </ul>
+  
+class App extends React.Component {
+
+  render(){
+    return(
+      <Router history={ hashHistory }>
+        <Route path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/contact" component={Contact}></Route>
+      </Router>
+    )
+  }
+}
 
 export default App;
